6b21371fab2a5412738884dc572facc7a5c8c30f
valderfield <huoruohe@163.com>
2019-06-02 16:11:54 +0800

    增加searchview 分享功能 暂未实现截图分享

--- app/src/main/java/com/field/weather/MyApp.java
+++ app/src/main/java/com/field/weather/MyApp.java
@@ -1,17 +1,25 @@
 package com.field.weather;
 
+import android.Manifest;
 import android.app.Application;
+import android.os.Build;
 
 import org.litepal.LitePal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 初始化用
  */
 public class MyApp extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
         //初始化数据库
         LitePal.initialize(this);
     }
+
+
 }
--- app/src/main/java/com/field/weather/fragment/WeatherFragment.java
+++ app/src/main/java/com/field/weather/fragment/WeatherFragment.java
@@ -1,24 +1,39 @@
 package com.field.weather.fragment;
 
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.field.weather.R;
@@ -35,6 +50,7 @@ import com.google.gson.Gson;
 import org.litepal.LitePal;
 import org.litepal.crud.callback.FindCallback;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -54,6 +70,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 /**
  * A simple {@link Fragment} subclass.
  * 显示具体天气信息界面 fragment
+ * 1.添加搜索功能
+ * 2. 暂时不显示 出现bug 代码逻辑有问题 添加拍照 记录当前天气 修改天气背景 修改布局添加 拍照按钮
+ * 3.语音识别 显示天气
+ * 4.toolbar 增加分享天气功能
  */
 public class WeatherFragment extends Fragment {
     List<String> questionList = new ArrayList<>();
@@ -77,6 +97,20 @@ public class WeatherFragment extends Fragment {
     //public DrawerLayout mDrawerLayout;
     //搜索框
     private SearchView mSearchView;
+    //权限
+    //private String[] mPermissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CAMERA};
+    //private List<String> mUnPermissions = new ArrayList<>();
+    //拍照
+    //private Button mTakePictureButton;
+    //request code 拍照 权限
+//    public static final int CAMERA_CODE = 100;
+//    public static final String PICTURE_AUTHORITY = "com.field.fileprovider";
+//    public static final int PERMISSION_CODE = 200;
+//    //File
+//    private File mImageFile;
+//    private Uri mImageUri;
+        //分享功能
+    private Toolbar mToolbar;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -103,16 +137,57 @@ public class WeatherFragment extends Fragment {
         mSwipeRefresh = view.findViewById(R.id.swipe_refresh);
         //mDrawerLayout = view.findViewById(R.id.drawer_layout);
         mSearchView = view.findViewById(R.id.area_search_view);
-        //设置searchview 属性
+        //设置search view 属性
+        //take picture button
+//        mTakePictureButton = view.findViewById(R.id.take_picture_button);
+        //toolbar
+        mToolbar = view.findViewById(R.id.weather_fragment_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
+        setHasOptionsMenu(true);
 
         mSearchView.setSubmitButtonEnabled(true);
         mSearchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
         return view;
     }
+
+    //menu
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //实例化menu
+        menu.clear();//清楚activity的menu
+        inflater.inflate(R.menu.weather_fragment_toolbar_menu,menu);
+    }
+    //分享天气信息 最好实现截屏分享
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toolbar_share:
+                // intent.setType("text/plain"); //纯文本
+                /*
+                 * 图片分享 it.setType("image/png"); 　//添加图片 File f = new
+                 * File(Environment.getExternalStorageDirectory()+"/name.png");
+                 *
+                 * Uri uri = Uri.fromFile(f); intent.putExtra(Intent.EXTRA_STREAM,
+                 * uri);
+                 */
+                Intent intent=new Intent(Intent.ACTION_SEND);
+                //intent.setType("image/*");
+                intent.setType("text/plain");
+                //intent.putExtra(Intent.EXTRA_SUBJECT, "Share");
+                intent.putExtra(Intent.EXTRA_TEXT, "分享天气信息");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(Intent.createChooser(intent, "分享"));
+                return true;
+        }
+        return false;
+    }
+
     //此处处理逻辑
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        //添加toolbar
 
         //先读取本地 如果没有就 网络请求然后回调
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
@@ -145,31 +220,43 @@ public class WeatherFragment extends Fragment {
                 requestWeather(mWeatherId);
             }
         });
-        //搜索框设置监听器
+        //搜索框设置监听器此处逻辑有问题 出现bug 原因是 数据库没有加载所有的 数据
         mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 //当点击搜索提交时
-                //先获取所有的城市名字存入string数组
-                //然后判断 s是否包含在 string数组中
+                //先获取所有的城市名字存入string数组 然后判断 如果没有查询到 就return false 出现问题 只显示区域
+//                List<String> names = new ArrayList<>();
+//                List<County> allCounties = LitePal.findAll(County.class);
+//                for (County county : allCounties) {
+//                    names.add(county.getCountyName());
+//                    Log.d(TAG, "onQueryTextSubmit: " + county.getCountyName());
+//                }
+//
+////                然后 判断 s是否包含在 string数组中
+//                if (names.contains(s)){
+                    LitePal.where("countyName=?",s).findFirstAsync(County.class)
+                            .listen(new FindCallback<County>() {
+                                @Override
+                                public void onFinish(County county) {
+                                    requestWeather(county.getWeatherId());
 
+                                }
+                            });
 
-                LitePal.where("countyName=?",s).findFirstAsync(County.class)
-                        .listen(new FindCallback<County>() {
-                            @Override
-                            public void onFinish(County county) {
-                                requestWeather(county.getWeatherId());
+                     //查询完毕后 清除搜索内容
+                     mSearchView.setQuery("",false);
+                    //查询完毕默认失去焦点
+                     mSearchView.setFocusable(false);
+//                }else {
+//                    Toast.makeText(getContext(), "输入的城市不存在,请重新输入", Toast.LENGTH_SHORT).show();
+//                    //把事件消费掉
+//                    return false;
+//                }
 
-                            }
-                        });
-
-                //查询完毕后 清除搜索内容
-                mSearchView.setQuery("",false);
-                //查询完毕默认失去焦点
-                mSearchView.setFocusable(false);
                 return false;
             }
-            //当搜索内容更新时
+            //当搜索内容更新
             @Override
             public boolean onQueryTextChange(String s) {
                 //只需要将获取的字符串 传入条件 查询的必然是想要的地区
@@ -185,7 +272,65 @@ public class WeatherFragment extends Fragment {
                 return false;
             }
         });
+
+//        //take picture
+//        mTakePictureButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //permissions
+//                getPermissions();
+//                //file
+//                mImageFile = new File(getActivity().getExternalCacheDir(),"weather.jpg");
+//                //uri
+//                mImageUri = FileProvider.getUriForFile(getActivity(),PICTURE_AUTHORITY,mImageFile);
+//                //intent
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                intent.putExtra(MediaStore.EXTRA_OUTPUT,mImageUri);
+//                startActivityForResult(intent,CAMERA_CODE);
+//            }
+//        });
     }
+//
+//    private void getPermissions() {
+//        mUnPermissions.clear();
+//        //遍历是否授权
+//        for (int i = 0; i < mPermissions.length; i++) {
+//            if (ContextCompat.checkSelfPermission(getActivity(),mPermissions[i]) != PackageManager.PERMISSION_GRANTED){
+//                mUnPermissions.add(mPermissions[i]);
+//            }
+//        }
+//        //申请权限
+//        if (mUnPermissions.size() > 0){
+//            ActivityCompat.requestPermissions(getActivity(), (String[]) mUnPermissions.toArray(),PERMISSION_CODE);
+//        }
+//    }
+//    //处理授权结果
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode){
+//            case PERMISSION_CODE:
+//                for (int i = 0; i < grantResults.length; i++) {
+//                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+//                        //处理 同意的时候
+//                    }
+//                }
+//                break;
+//                default:
+//                    break;
+//        }
+//    }
+//    //处理返回的result
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        switch (requestCode){
+//            case CAMERA_CODE:
+//                if (resultCode == getActivity().RESULT_OK){
+//                    //更新背景
+//                    //Glide.with(getActivity()).load(mImageUri).into(mBingPicImage);
+//                }
+//        }
+//    }
 
     //网络请求bingPic
     public void loadBingPic() {
